9f80594fa6910a88f9b2747a36fc35871e2a5fa1
Elmomo <test@test.com>
2022-04-22 18:46:10 -0400

    change modal scroll

--- pages/index.tsx
+++ pages/index.tsx
@@ -42,7 +42,8 @@ const Home = ({
   if(load) return null
   
   return (
-    <div className="relative h-screen bg-gradient-to-b  lg:h-[140vh]">
+    <div className={`relative h-screen bg-gradient-to-b  lg:h-[140vh]
+    ${showModel && '!h-screen overflow-hidden'}`}>
       <Head>
         <title>Movie - API</title>
         <link rel="icon" href="/favicon.ico" />
